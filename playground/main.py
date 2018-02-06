b45609a830e558d8f8fa13ece6f789928e835fb4
Marco Radic <marco.radic@live.de>
2018-02-06 13:29:45 +0100

    fix wrong filepath

--- playground/main.py
+++ playground/main.py
@@ -4,7 +4,7 @@ from networks.pos_bilstm import POSModel
 import numpy as np
 
 def main():
-    #util.load_glove_pkl(os.path.join(os.getenv("DATA_DIR_DL"), str('word2vec/glove.pkl')))
+    util.load_glove_pkl(os.getenv("DATA_DIR_DL")+str('word2vec/glove.pkl'))
     util.load_glove_txt(os.getenv("DATA_DIR_DL")+str('/word2vec/glove.840B.300d.w2vformat.txt'))
     sentences_train, lexicon_dic, classes = util.parse_pos(os.getenv("DATA_DIR_DL")+'/pos/en-train.txt')
     sentences_val, lexikon_val_dic, classes_val = util.parse_pos(os.getenv("DATA_DIR_DL")+'/pos/en-test.txt')
